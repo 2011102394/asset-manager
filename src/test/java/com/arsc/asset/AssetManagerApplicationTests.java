262ef95a51d15f8105676e231819a7dafaccd3ca
zhangcj <602173141@qq.com>
2026-01-04 16:28:43 +0800

    feat: 项目初始化

---
+++ src/test/java/com/arsc/asset/AssetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsc.asset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
